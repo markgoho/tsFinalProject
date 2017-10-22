deb813743df49ccea7ef067b7bb1a03d56ffebb2
Mark Goho <markgoho@gmail.com>
2017-10-21 21:20:54 -0400

    add another test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,20 +1,22 @@
 import { isBoolean } from './index';
 
-test('it should return true if the given value is a boolean', () => {
-  expect(isBoolean(new Boolean())).toEqual(true);
-});
+describe('isBoolean()', () => {
+  test('it should return true if the given value is a boolean', () => {
+    expect(isBoolean(new Boolean())).toEqual(true);
+  });
 
-test('it should return true if the given value is literally `true`', () => {
-  expect(isBoolean(true)).toEqual(true);
-});
+  test('it should return true if the given value is literally `true`', () => {
+    expect(isBoolean(true)).toEqual(true);
+  });
 
-test('it should return true if the given value is literally `false`', () => {
-  expect(isBoolean(true)).toEqual(true);
-});
+  test('it should return true if the given value is literally `false`', () => {
+    expect(isBoolean(true)).toEqual(true);
+  });
 
-test('it should return false if the given value is any non-boolean value', () => {
-  expect(isBoolean('')).toEqual(false);
-  expect(isBoolean(undefined)).toEqual(false);
-  expect(isBoolean(null)).toEqual(false);
-  expect(isBoolean(123)).toEqual(false);
+  test('it should return false if the given value is any non-boolean value', () => {
+    expect(isBoolean('')).toEqual(false);
+    expect(isBoolean(undefined)).toEqual(false);
+    expect(isBoolean(null)).toEqual(false);
+    expect(isBoolean(123)).toEqual(false);
+  });
 });
