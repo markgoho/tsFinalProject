0e4734c1aab6fbe5a14dc7cfa0358178d052cb34
Mark Goho <markgoho@gmail.com>
2017-10-21 20:58:40 -0400

    add more tests

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+const sourceMaps = require('rollup-plugin-sourcemaps');
+const packageJSON = require('./package.json');
+
+export default {
+  input: 'dist/index.js',
+  output: [
+    {
+      file: packageJSON.main,
+      format: 'umd',
+      name: 'typeScriptProLibraryProject'
+    }
+  ],
+  sourcemap: true,
+  plugins: [
+    sourceMaps()
+  ]
+}
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,20 @@
-import lib from './index'
+import { isBoolean } from './index';
 
-test('it should work', () => {
-  expect(true).toEqual(true)
-  console.log(lib)
-})
+test('it should return true if the given value is a boolean', () => {
+  expect(isBoolean(new Boolean())).toEqual(true);
+});
+
+test('it should return true if the given value is literally `true`', () => {
+  expect(isBoolean(true)).toEqual(true);
+});
+
+test('it should return true if the given value is literally `false`', () => {
+  expect(isBoolean(true)).toEqual(true);
+});
+
+test('it should return false if the given value is any non-boolean value', () => {
+  expect(isBoolean('')).toEqual(false);
+  expect(isBoolean(undefined)).toEqual(false);
+  expect(isBoolean(null)).toEqual(false);
+  expect(isBoolean(123)).toEqual(false);
+});
